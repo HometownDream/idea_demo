e4c587ae70b5bc637eb496ad742c118575a3d28b
zyk <778268716@qq.com>
2024-08-21 16:52:22 +0800

    优化了模块2的开发

--- src/main/java/com/atguigu/module2.java
+++ src/main/java/com/atguigu/module2.java
@@ -1,6 +1,6 @@
 package com.atguigu;
 
-public class module {
+public class module2 {
     public static void main(String[] args) {
         System.out.println("完成模块2的开发");
         System.out.println("优化了模块2的开发");
